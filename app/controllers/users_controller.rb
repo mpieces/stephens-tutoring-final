4a947bfde897032168a087375d599d4d32635904
Marisa Ramon <mer0522@gmail.com>
2016-02-19 17:13:41 -0600

    Add signup form, new and create methods in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    User.create(params[:user_params])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
