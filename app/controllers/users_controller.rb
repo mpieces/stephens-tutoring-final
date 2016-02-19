76e41c623e3380e5352104cb6a3a4d442aab65b0
Marisa Ramon <mer0522@gmail.com>
2016-02-19 16:13:42 -0600

    Add secure passwords to User model, fix whitespace in front of one of validates methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  def create
+    User.create(params[:user_params])
+  end
+
+  # def update
+
+
+  private
+
+    def user_params
+      params.require(:email).permit(:first_name, :last_name, :phone, :address, :city, :zip, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
 class User < ActiveRecord::Base
   # attr_accessible :first_name, :last_name, :email, :phone, :address, :city
-
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }, uniqueness: { case_sensitive: false }
 #uniqueness inferred by Rails to be true
+
   validates :phone, presence: true
   validates :address, presence: true
   validates :city, presence: true
   validates :zip, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
